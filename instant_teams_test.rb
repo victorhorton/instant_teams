b2736e0e220f482728af8e130282ce7c838624d5
Victor Horton <vichorton5@gmail.com>
2023-01-10 22:37:11 -0600

    Add sets with projects

--- instant_teams_test.rb
+++ instant_teams_test.rb
@@ -2,3 +2,70 @@ travel_low_cost = 45
 travel_high_cost = 55
 full_low_cost = 75
 full_high_cost = 85
+
+set_1 = [
+	{
+		city_type: 'low_cost',
+		start_date: '9/1/15',
+		end_date: '9/3/15'
+	}
+]
+
+set_2 = [
+	{
+		city_type: 'low_cost',
+		start_date: '9/1/15',
+		end_date: '9/1/15',
+	},
+	{
+		city_type: 'high_cost',
+		start_date: '9/2/15',
+		end_date: '9/6/15',
+	},
+	{
+		city_type: 'low_cost',
+		start_date: '9/6/15',
+		end_date: '9/8/15',
+	}
+]
+
+set_3 = [
+	{
+		city_type: 'low_cost',
+		start_date: '9/1/15',
+		end_date: '9/3/15',
+	},
+	{
+		city_type: 'high_cost',
+		start_date: '9/5/15',
+		end_date: '9/7/15',
+	},
+	{
+		city_type: 'high_cost',
+		start_date: '9/8/15',
+		end_date: '9/8/15',
+	}
+]
+
+set_4 = [
+	{
+		city_type: 'low_cost',
+		start_date: '9/1/15',
+		end_date: '9/1/15',
+	},
+	{
+		city_type: 'low_cost',
+		start_date: '9/1/15',
+		end_date: '9/1/15',
+	},
+	{
+		city_type: 'high_cost',
+		start_date: '9/2/15',
+		end_date: '9/2/15',
+	},
+	{
+		city_type: 'high_cost',
+		start_date: '9/2/15',
+		end_date: '9/3/15',
+	}
+]
